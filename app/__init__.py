e5d13faf677de1ffb2a1bca157780308d9516891
Olga-allgood <olga.s.orlova@gmail.com>
2025-10-13 06:19:06 -0700

    read all books end point

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
+# __init__.py
+
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+from app.routes.book_routes import book_bp
 
 def create_app():
     app = Flask(__name__)
 
-    # Register blueprints
-    app.register_blueprint(hello_world_bp)
+    # Register Blueprints here
+    app.register_blueprint(book_bp)
 
     return app
